b8438103ed2624d3f1a38cd38d099bc8896c6e6d
Bogdan Kiselev <dtx1212@gmail.com>
2022-02-10 19:45:00 +0300

    remove useless context switching

--- app/src/main/java/ru/dtx12/coolblue/core/interactor/UseCase.kt
+++ app/src/main/java/ru/dtx12/coolblue/core/interactor/UseCase.kt
@@ -8,10 +8,8 @@ abstract class UseCase<out Type, in Params> {
     protected abstract suspend fun run(params: Params): Type
 
     suspend fun execute(params: Params): Type {
-        return withContext(Dispatchers.Main.immediate) {
-            withContext(Dispatchers.IO) {
-                run(params)
-            }
+        return withContext(Dispatchers.IO) {
+            run(params)
         }
     }
 
